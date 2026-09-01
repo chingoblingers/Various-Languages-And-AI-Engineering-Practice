576319e78351ae8e2b46efcb601a91c997ae5a8b
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-08-31 19:21:53 -0500

    updated python server response

--- src/practiceFiles/signalDesk.py
+++ src/practiceFiles/signalDesk.py
@@ -6,5 +6,11 @@ class  DiagnosticRequest(BaseModel):
     message: str
 @app.post('/diagnostics')
 async def runDiagnostics(request:DiagnosticRequest):
-    message = request.message
-    return {'diagnostic': message }
+    message = request.message.lower()
+    if "econnrefused" in message:
+        return {"diagnostic": "This could be a connection error due to..."}
+    elif 'timeout' in message:
+        return {"diagnostic": "This could be a issue with the server due to..."}
+    else:
+        return {"diagnostic": "Unable to identify a known diagnostic pattern."}
+    
