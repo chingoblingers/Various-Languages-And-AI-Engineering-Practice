9fce70faf33e4d302f678623f0e2c82b447319b3
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-01 23:14:51 -0500

    created find closest vector function

--- src/practiceFiles/questSupport.py
+++ src/practiceFiles/questSupport.py
@@ -14,4 +14,11 @@ def search(userRequest: SearchRequest):
    query_vector = embed_query(userRequest.query)
     return {"query": userRequest.query, "results": []}
 
+stored_vectors = [
+    [0.1, 0.2, 0.2],
+    [0.8, 0.7, 0.9],
+    [0.0, 0.1, 0.2]
+]
 
+def find_closest_vector(query_vector: list[float]) -> list[float]:
+        return stored_vectors[0]
