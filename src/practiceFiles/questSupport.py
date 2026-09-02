b2ed4cfea7b815875251d6fe7a862a2dc4a4be7c
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-02 00:15:52 -0500

    refactored vector function and search function

--- src/practiceFiles/questSupport.py
+++ src/practiceFiles/questSupport.py
@@ -13,13 +13,16 @@ def embed_query(query:str) -> list[float]:
 def search(userRequest: SearchRequest):
    query_vector = embed_query(userRequest.query)
    closest_vector = find_closest_vector(query_vector)
-    return {"query": userRequest.query, "results": closest_vector}
+   results = [] if closest_content is None else [closest_content]
+    return {"query": userRequest.query, "results": closest_content}
 
 stored_vectors = [
-    [0.1, 0.2, 0.2],
-    [0.8, 0.7, 0.9],
-    [0.0, 0.1, 0.2]
+    {"content": "We are the champions", "embedding": [0.1, 0.2, 0.2]},
+    {"content": "Hello World", "embedding": [0.8, 0.7, 0.9]},
+    {"content": "Bacon pancakes", "embedding": [0.0, 0.1, 0.2]}
 ]
 
-def find_closest_vector(query_vector: list[float]) -> list[float]:
-        return stored_vectors[0]
+def find_closest_vector(query_vector: list[float]) -> str|None:
+        for document in stored_vectors:
+            if query_vector == document['embedding']:
+                return document["content"]
