b4b792f5b0ad0ff0280e719da12324d4d3b274cf
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-01 22:52:20 -0500

    created quest support python server

---
+++ src/practiceFiles/questSupport.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class SearchRequest(BaseModel):
+    query:str
+
+@app.post("/search")
+def search(userRequest: SearchRequest):
+    return {"query": userRequest.query, "results": []}
+
+
