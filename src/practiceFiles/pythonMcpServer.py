da99dcb3c04a0123e7ea60c5de61194c5e4160a2
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-05 08:09:22 -0500

    created return teplate for the server

--- src/practiceFiles/pythonMcpServer.py
+++ src/practiceFiles/pythonMcpServer.py
@@ -5,3 +5,12 @@ app = FastAPI()
 
 class RequirementsRequest(BaseModel):
     game: str
+
+@app.post('/requirements')
+def systemRequirements(game:RequirementsRequest):
+    if game.game == 'Dragon\'s Dogma 2':
+        return {'requirements': "dd2 requirements"}
+    else:
+        return {'requirements': 'requirements not found'}
+
+   
